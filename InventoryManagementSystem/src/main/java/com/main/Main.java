a210a5f2ad4df716d91b6e46c5d688255b6537d6
Leo Deg <rdkdvv@gmail.com>
2019-11-25 20:13:33 +0300

    feat: start build user interface

--- InventoryManagementSystem/src/main/java/com/main/Main.java
+++ InventoryManagementSystem/src/main/java/com/main/Main.java
@@ -1,56 +1,21 @@
 package com.main;
 
-import com.main.model.entity.BeginEntity;
-import com.main.model.entity.ProductEntity;
-import com.main.model.jpa.JpaBeginDao;
-import com.main.model.jpa.JpaProductDao;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.util.Date;
-import java.util.TimeZone;
-
-//public class Main extends Application {
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
-
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        CreateBeginEntity();
-//        UpdateProduct();
+        launch(args);
     }
 
-
-
-    private static void CreateBeginEntity() {
-        JpaBeginDao beginDao = new JpaBeginDao();
-        BeginEntity beginEntity = new BeginEntity(37, 10, 15.00, new Date());
-        System.out.print(beginEntity.toString());
-        beginDao.save(beginEntity);
-        beginDao.close();
-    }
-
-    private static void UpdateProduct () {
-        JpaProductDao productDao = new JpaProductDao();
-
-        ProductEntity productEntity = productDao.get(36).get();
-        System.out.print("\n\n");
-        System.out.print(productEntity.toString());
-        System.out.print("\n\n");
-        productDao.update(productEntity, new String[] {"1", "Computer", "15.0", "Computer"});
-
-        productEntity = productDao.get(36).get();
-        System.out.print("\n\n");
-        System.out.print(productEntity.toString());
-        System.out.print("\n\n");
-        productDao.close();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/main.fxml"));
+        primaryStage.setTitle("Inventory management system");
+        primaryStage.setScene(new Scene(root, 700, 500));
+        primaryStage.show();
     }
 }
